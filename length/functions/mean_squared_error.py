a2e01989f9be8b25fd3a97790dd9a8a19a5c3cd2
CodeLionX <sebastian.schmidl@t-online.de>
2018-05-02 18:11:39 +0200

    mse back and forward

--- length/functions/mean_squared_error.py
+++ length/functions/mean_squared_error.py
@@ -21,7 +21,12 @@ class MeanSquaredError(Function):
         # Example: assume input is the following: [2, 3], and shape is (2, 4)
         # the resulting vector should look like this:
         # result = [[0, 0, 1, 0], [0, 0, 0, 1]]
-        return None
+        # see: https://stackoverflow.com/questions/29831489/numpy-1-hot-array
+        one_hot = np.zeros(shape)
+        one_hot[np.arange(shape[0]), data] = 1
+
+        assert one_hot.shape == shape, "one_hot shape does not fit"
+        return one_hot
 
     def internal_forward(self, inputs):
         x1, x2 = inputs
@@ -30,17 +35,27 @@ class MeanSquaredError(Function):
             x2 = self.create_one_hot(x2, x1.shape)
 
         # TODO: calculate the mean squared error of x1 and x2
-        error = 0.0
+        error = np.array([np.mean(np.sum(np.square(x1i - x2i))) for x1i, x2i in zip(x1, x2)])
+        print("rmse: " + str(error))
 
-        return error
+        return error,
 
     def internal_backward(self, inputs, gradients):
         x1, x2 = inputs
         gx, = gradients
-        # TODO: calculate the gradients of this function with respect to its inputs
-        gradient_1 = np.zeros_like(x1)
-        gradient_2 = np.zeros_like(x2)
 
+        # TODO: calculate the gradients of this function with respect to its inputs
+        if np.issubdtype(x2.dtype, np.integer):
+            x2 = self.create_one_hot(x2, x1.shape)
+
+        print("backward input x1.shape: " + str(x1.shape))
+        print("backward input x2.shape: " + str(x2.shape))
+
+        gradient_1 = np.array([2 * np.mean(np.sum(x1i - x2i)) for x1i, x2i in zip(x1, x2)]) # d MSE / d x1
+        gradient_2 = np.array([2 * np.mean(np.sum(x2i - x1i)) for x1i, x2i in zip(x1, x2)]) # d MSE / d x2
+
+        print("grad x1: " + str(gradient_1))
+        print("grad x2: " + str(gradient_2))
         if np.issubdtype(x2.dtype, np.integer):
             # in case we used MSE as loss function, we won't propagate any gradients to the labels
             return gradient_1, None

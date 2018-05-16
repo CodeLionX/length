04649fe4a6a82d3659771bbc7af4c06c6efafbb4
CodeLionX <sebastian.schmidl@t-online.de>
2018-05-16 09:54:56 +0200

    implemented adam optimizer

--- length/optimizers/adam.py
+++ length/optimizers/adam.py
@@ -15,12 +15,30 @@ class Adam(Optimizer):
     def __init__(self, learning_rate=0.001, beta1=0.9, beta2=0.999, eps=1e-8):
         self.learning_rate = learning_rate
         self.beta1 = beta1
+        self.ibeta1 = 1 - beta1
         self.beta2 = beta2
+        self.ibeta2 = 1 - beta2
         self.eps = eps
 
         # TODO: add more initialization code
+        self.t = 0
+        self.m = None
+        self.v = None
+        self.isInit = True
 
     def run_update_rule(self, gradients, layer):
         # TODO: implement Adam update rule as specified in https://arxiv.org/abs/1412.6980
-        param_deltas = [np.zeros_like(g) for g in gradients]
-        return param_deltas
+        gradient, = gradients
+
+        if self.isInit:
+            self.m = np.full(gradient.shape, 0)
+            self.v = np.full(gradient.shape, 0)
+            self.isInit = False
+
+        self.t += 1
+        self.m = self.beta1 * self.m + self.ibeta1 * gradient
+        self.v = self.beta2 * self.v + self.ibeta2 * np.square(gradient)
+        m_corr = self.m / (1 - np.power(self.beta1, self.t))
+        v_corr = self.v / (1 - np.power(self.beta2, self.t))
+        delta = self.learning_rate * m_corr / (np.sqrt(v_corr) + self.eps)
+        return delta,
